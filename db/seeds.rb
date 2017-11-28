477acb0dbb2c31e67a761b7d95903b9f076d5eab
Nick Martinez <23nicholasmartinez@gmail.com>
2017-11-28 11:50:55 -0800

    Add Places data with Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+center_point = { lat: 50.0515918, lng: 19.9357531 }
+
+1.upto(1000) do |i|
+  Place.create!(
+    name: Faker::Address.city,
+    description: Faker::Lorem.paragraph(8),
+    longitude: center_point[:lng] + rand(-10.00..10.00),
+    latitude: center_point[:lat] + rand(-10.00..10.00),
+    price: rand(1..500)
+  )
+end
